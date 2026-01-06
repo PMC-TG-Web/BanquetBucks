192c696ef213d7dff0f61fc98e338f89bd5278c8
Todd Gilmore <todd@pmcdecor.com>
2026-01-06 08:08:56 -0500

    Add auction items column with auto-incrementing item numbers

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
 // Data storage
 let participants = [];
+let auctionItemCounter = 1;
 
 // Initialize with sample data
 function initializeSampleData() {
     // Real participants from employee list
     participants = [
-        { name: "Alvin Huyard", hireDate: "11/13/2017", years: 8.15, bucks: 2534, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Ben Zook", hireDate: "2/27/2023", years: 2.86, bucks: 1992, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Dan Hess", hireDate: "6/17/2019", years: 6.56, bucks: 2189, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Danny Hess (Son)", hireDate: "3/18/2024", years: 1.81, bucks: 2190, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Danny Jones", hireDate: "5/24/2018", years: 7.63, bucks: 1978, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Dave Barrett", hireDate: "5/2/2022", years: 3.68, bucks: 1201, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Ervin Stoltzfus", hireDate: "3/27/2023", years: 2.78, bucks: 2138, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Francisco \"Poncho\" Romero", hireDate: "6/24/2024", years: 1.54, bucks: 2222, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Isaac Stoltzfus, Jr.", hireDate: "12/4/2012", years: 13.10, bucks: 2043, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Ivan Zavaleta", hireDate: "3/4/2024", years: 1.84, bucks: 2170, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "James King", hireDate: "1/30/2023", years: 2.94, bucks: 2091, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Jane Dropeskey", hireDate: "8/21/2023", years: 2.38, bucks: 1918, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Japheth King", hireDate: "7/14/2025", years: 0.48, bucks: 412, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Jason Stoltzfus", hireDate: "4/1/2019", years: 6.77, bucks: 2216, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "John King", hireDate: "3/26/2024", years: 1.78, bucks: 1990, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Jose Alpizar", hireDate: "5/13/2024", years: 1.65, bucks: 1768, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Jose Ivan Cruz", hireDate: "9/9/2024", years: 1.33, bucks: 1851, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Joseph Beiler", hireDate: "8/31/2020", years: 5.35, bucks: 2028, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Joshua Quidroz", hireDate: "7/24/2023", years: 2.46, bucks: 1747, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Lee Zook", hireDate: "8/4/2025", years: 0.42, bucks: 409, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Logan Spence", hireDate: "10/7/2024", years: 1.25, bucks: 1779, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Matt Beiler", hireDate: "6/15/2020", years: 5.56, bucks: 2067, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Matthew King", hireDate: "11/6/2018", years: 7.17, bucks: 1851, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Mervin Allgyer", hireDate: "3/16/2015", years: 10.82, bucks: 2040, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Moses Zook", hireDate: "5/16/2022", years: 3.65, bucks: 2099, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Omar Garcia Cruz", hireDate: "9/23/2024", years: 1.29, bucks: 1891, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Omar Stoltzfus", hireDate: "4/27/2017", years: 8.70, bucks: 1809, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Raymond King", hireDate: "4/23/2019", years: 6.71, bucks: 2094, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Scott Swinehart", hireDate: "10/28/2013", years: 12.20, bucks: 1941, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Shelly Swinehart", hireDate: "3/11/2015", years: 10.83, bucks: 1984, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "Todd Gilmore", hireDate: "9/2/2025", years: 0.35, bucks: 315, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
-        { name: "William Stoltzfus", hireDate: "6/14/2021", years: 4.57, bucks: 2205, toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false }
+        { name: "Alvin Huyard", hireDate: "11/13/2017", years: 8.15, bucks: 2534, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Ben Zook", hireDate: "2/27/2023", years: 2.86, bucks: 1992, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Dan Hess", hireDate: "6/17/2019", years: 6.56, bucks: 2189, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Danny Hess (Son)", hireDate: "3/18/2024", years: 1.81, bucks: 2190, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Danny Jones", hireDate: "5/24/2018", years: 7.63, bucks: 1978, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Dave Barrett", hireDate: "5/2/2022", years: 3.68, bucks: 1201, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Ervin Stoltzfus", hireDate: "3/27/2023", years: 2.78, bucks: 2138, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Francisco \"Poncho\" Romero", hireDate: "6/24/2024", years: 1.54, bucks: 2222, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Isaac Stoltzfus, Jr.", hireDate: "12/4/2012", years: 13.10, bucks: 2043, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Ivan Zavaleta", hireDate: "3/4/2024", years: 1.84, bucks: 2170, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "James King", hireDate: "1/30/2023", years: 2.94, bucks: 2091, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Jane Dropeskey", hireDate: "8/21/2023", years: 2.38, bucks: 1918, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Japheth King", hireDate: "7/14/2025", years: 0.48, bucks: 412, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Jason Stoltzfus", hireDate: "4/1/2019", years: 6.77, bucks: 2216, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "John King", hireDate: "3/26/2024", years: 1.78, bucks: 1990, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Jose Alpizar", hireDate: "5/13/2024", years: 1.65, bucks: 1768, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Jose Ivan Cruz", hireDate: "9/9/2024", years: 1.33, bucks: 1851, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Joseph Beiler", hireDate: "8/31/2020", years: 5.35, bucks: 2028, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Joshua Quidroz", hireDate: "7/24/2023", years: 2.46, bucks: 1747, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Lee Zook", hireDate: "8/4/2025", years: 0.42, bucks: 409, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Logan Spence", hireDate: "10/7/2024", years: 1.25, bucks: 1779, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Matt Beiler", hireDate: "6/15/2020", years: 5.56, bucks: 2067, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Matthew King", hireDate: "11/6/2018", years: 7.17, bucks: 1851, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Mervin Allgyer", hireDate: "3/16/2015", years: 10.82, bucks: 2040, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Moses Zook", hireDate: "5/16/2022", years: 3.65, bucks: 2099, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Omar Garcia Cruz", hireDate: "9/23/2024", years: 1.29, bucks: 1891, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Omar Stoltzfus", hireDate: "4/27/2017", years: 8.70, bucks: 1809, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Raymond King", hireDate: "4/23/2019", years: 6.71, bucks: 2094, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Scott Swinehart", hireDate: "10/28/2013", years: 12.20, bucks: 1941, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Shelly Swinehart", hireDate: "3/11/2015", years: 10.83, bucks: 1984, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "Todd Gilmore", hireDate: "9/2/2025", years: 0.35, bucks: 315, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false },
+        { name: "William Stoltzfus", hireDate: "6/14/2021", years: 4.57, bucks: 2205, auctionItems: [], toolbox: false, motivating: false, organized: false, safety: false, humorous: false, wordSearch: false, yardage: false }
     ];
 
     saveData();
@@ -46,9 +47,11 @@ function initializeSampleData() {
 // Load data from localStorage
 function loadData() {
     const savedParticipants = localStorage.getItem('participants');
+    const savedCounter = localStorage.getItem('auctionItemCounter');
     
     if (savedParticipants) {
         participants = JSON.parse(savedParticipants);
+        auctionItemCounter = savedCounter ? parseInt(savedCounter) : 1;
         return true;
     }
     return false;
@@ -57,6 +60,31 @@ function loadData() {
 // Save data to localStorage
 function saveData() {
     localStorage.setItem('participants', JSON.stringify(participants));
+    localStorage.setItem('auctionItemCounter', auctionItemCounter.toString());
+}
+
+// Award auction item to participant
+function awardAuctionItem(index) {
+    const participant = participants[index];
+    
+    const price = parseInt(prompt(`Enter price for Auction Item #${auctionItemCounter}:`));
+    
+    if (price === null || isNaN(price) || price <= 0) return;
+    
+    if (participant.bucks < price) {
+        alert(`${participant.name} doesn't have enough bucks! They have ${participant.bucks}, but need ${price}.`);
+        return;
+    }
+    
+    participant.auctionItems = participant.auctionItems || [];
+    participant.auctionItems.push(auctionItemCounter);
+    participant.bucks -= price;
+    
+    auctionItemCounter++;
+    
+    saveData();
+    renderParticipants();
+    updateStats();
 }
 
 // Add a participant
@@ -220,6 +248,12 @@ function renderParticipants() {
                 <div class="action-buttons">
                     <button class="adjust-btn" onclick="adjustBucks(${index}, 'custom')">Adjust Bucks</button>
                 </div>
+            </td>
+            <td style="text-align: center;">
+                <button class="action-btn" onclick="awardAuctionItem(${index})">Award Item</button>
+                ${participant.auctionItems && participant.auctionItems.length > 0 ? 
+                    `<div style="margin-top: 5px; font-size: 12px;">${participant.auctionItems.join(', ')}</div>` : 
+                    ''}
             </td>
             <td style="text-align: center;">
                 ${participant.toolbox ? 
